36763e92e6dfdeef9042ef78bd35ba915c6eca1e
Siva <sivasubrahmanyam.pagolu@techwave.net>
2022-11-30 22:01:11 +0530

    added @Parameter annotation

--- src/main/java/com/oneoonecode/swaggercrashcourse/controllers/UpdateToDoItem.java
+++ src/main/java/com/oneoonecode/swaggercrashcourse/controllers/UpdateToDoItem.java
@@ -2,6 +2,7 @@ package com.oneoonecode.swaggercrashcourse.controllers;
 
 import com.oneoonecode.swaggercrashcourse.models.ToDoItem;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,7 +10,12 @@ import org.springframework.web.bind.annotation.*;
 public class UpdateToDoItem {
     @PutMapping(value = "/todos/{toDoItemId}")
     @Operation(
-       tags = {"TodoItems","TodoItems 2"}
+            tags = {"TodoItems","TodoItems 2"},
+            operationId = "updatedTodo",
+            summary = "This is summary",
+            description = "This is description",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "This is request body description"),
+            parameters = @Parameter(name = "toDoItemId", description = "The path variable")
     )
     public ResponseEntity<Object> updateToDoItem(@PathVariable String toDoItemId,
                                                  @RequestBody ToDoItem toDoItem,
